796109a918c467afed08749988f94f3db44b4b8d
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-28 16:34:17 +0530

    basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="fixed mx-auto w-fit">
+        <li className="flex flex-row gap-60 pt-6 text-3xl">
+            <button  className="cursor-pointer" onClick={useNavigate("/")}>Home</button>
+            <button  className="cursor-pointer" onClick={useNavigate("/#events")}>Events</button>
+            <button className="cursor-pointer" onClick={useNavigate("/#about")}>About Us</button>
+            <button className="cursor-pointer" onClick={useNavigate("/register")}>Register</button>
+        </li>
+    </nav>
   )
 }
 
