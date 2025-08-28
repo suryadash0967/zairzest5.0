ff8064f240e9015306808de1fd7648f0da786ecb
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-28 14:15:36 +0530

    Clean working directory for dev branch

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <Routes>
+          
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
