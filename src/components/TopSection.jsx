61b4cfd6604d27be690954a6a990265fecf2970e
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-29 12:39:04 +0530

    some changes in hero/top section

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -14,8 +14,22 @@ const TopSection = () => {
       }}
     >
         <img src="/images/girlBack.png" alt="Baddie" className="relative mx-auto h-[100vh] z-20"/>
-        <img src="/images/zairzestUnfill.png" alt="" className='absolute top-[15%] md:h-[350px] left-[10%] right-[10%] z-40' />
-        <img src="/images/zairzestFill.png" alt="" className='absolute top-[15%] md:h-[350px] left-[10%] z-10' />
+        <div id='zairzest' className='absolute  top-0 w-full h-full text-center z-10 lg:text-[360px] md:text-[200px] xl:text-[400px]  '>
+            ZAIRZEST
+        </div>
+        <div id='zairzestunfill' className='absolute  top-0 w-full h-full text-center z-20 md:text-[200px] lg:text-[360px] xl:text-[400px] '>
+            ZAIRZEST
+        </div>
+        <div className='absolute  top-[70%] w-full h-full text-center z-20 '>
+        <div className='b text-white w-[380px] px-3'>
+
+            The blueprints burned. Yet the grid hums, the circuits breathe, and the execution never halts.What remains are echoes, recursive loops of will and logic, ghosts of commands that refuse to fade. The Code still runs. 
+            <span className='bg-white text-red-500'>
+
+            Scroll to Join The Echo
+            </span>
+        </div>
+        </div>
     </div>
   )
 }
