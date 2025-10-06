883b35917e356305a3847612c1f79d91b8c72f1d
pandarudra <rudrapanda8206@gmail.com>
2025-10-06 11:40:01 +0530

    login signup be int. added

--- src/env/e.js
+++ src/env/e.js
@@ -1,1 +1,4 @@
-export const be_url = import.meta.env.VITE_BE_URL;
+export const be_url =
+  import.meta.env.VITE_ENV === "development"
+    ? "http://localhost:4000"
+    : import.meta.env.VITE_BE_URL;
