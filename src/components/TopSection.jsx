b6131695e39451fe1448453c6487545695915ab3
suryadash0967 <surya.dash0967@gmail.com>
2025-10-09 16:36:38 +0530

    fonts changes

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -5,17 +5,18 @@ const TopSection = () => {
   return (
     <div
       style={{
-        width: '100%',
+        width: '100vw',
         height: '100vh',
         backgroundImage: 'url(/images/bgTop.png)',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         position: 'relative',
+        overflowX: 'hidden',
       }}
     >
       <LogoSequence />
-      <img src="/images/girlBack.png" alt="Baddie" className="relative mx-auto h-screen z-20" />
+      <img src="/images/girlBack.png" alt="Baddie" className="hero-section-img" />
 
       <div className='absolute inset-0 flex items-center flex-col justify-between px-auto' >
 
@@ -30,7 +31,7 @@ const TopSection = () => {
           v.fiv<span className='text-red-500'>5</span>
         </div>
         <div id="topb" className='flex flex-col-reverse lg:flex-row absolute top-3/4 left-0 right-0 justify-center gap-40 items-center z-30'>
-          <div id="yap" className='text-left text-white w-[380px] md:w-[430px] backdrop-blur-2xl z-30 border-1 border-white/40 p-3 rounded-xl'>
+          <div id="yap" className='text-left text-white w-[430px] md:w-[430px] backdrop-blur-2xl z-30 border-1 border-white/40 p-3 rounded-xl'>
             The blueprints burned. Yet the grid hums, the circuits breathe, and the execution never halts. What remains are echoes, recursive loops of will and logic, ghosts of commands that refuse to fade. The Code still runs.
             <span className='bg-white text-red-500'>
               <br />
